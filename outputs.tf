32bd1361af7ffdfe44d243b2895ed7c27953b4ae
Robert de Bock <robert@meinit.nl>
2021-02-24 14:42:17 +0100

    Error: Reference to undeclared resource

--- outputs.tf
+++ outputs.tf
@@ -1,19 +1,19 @@
 output "name" {
   description = "The name of the resource group."
-  value       = azurerm_resource_group.rg.name
+  value       = azurerm_resource_group.default.name
 }
 
 output "location" {
   description = "The location of the resource group."
-  value       = azurerm_resource_group.rg.location
+  value       = azurerm_resource_group.default.location
 }
 
 output "id" {
   description = "The id of the resource group."
-  value       = azurerm_resource_group.rg.id
+  value       = azurerm_resource_group.default.id
 }
 
 output "tags" {
   description = "The tags of the resource group."
-  value       = azurerm_resource_group.rg.tags
+  value       = azurerm_resource_group.default.tags
 }
