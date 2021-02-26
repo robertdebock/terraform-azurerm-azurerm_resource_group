c9f307c60a5c65dabb08db9dc09ebc3dde0a98aa
Robert de Bock <robert@meinit.nl>
2021-02-26 15:13:17 +0100

    Enable tags.

--- examples/default/main.tf
+++ examples/default/main.tf
@@ -1,11 +1,11 @@
 module "azurerm_resource_group" {
-  source   = "../../"
-  name     = "test_resource_group"
-  location = "west europe"
-  # tags     = {
-  #   environment = "test"
-  #   name        = "my_rg"
-  # }
+  source        = "../../"
+  name          = "test_resource_group"
+  location      = "west europe"
+  tags          = {
+    environment = "test"
+    name        = "my_rg"
+  }
 }
 
 output "all" {
